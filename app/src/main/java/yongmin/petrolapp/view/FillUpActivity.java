9e5b2846adef51565dc460a83025365b184f42ca
yongmin park <yongmin.txt@gmail.com>
2016-12-05 14:13:53 +0000

    Initial commit

---
+++ app/src/main/java/yongmin/petrolapp/view/FillUpActivity.java
@@ -0,0 +1,14 @@
+package yongmin.petrolapp.view;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+import android.view.View;
+
+public class FillUpActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_fill_up);
+    }
+}
